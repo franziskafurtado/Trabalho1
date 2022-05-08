2b981ca5ec4d950e11f97b6d2de0edb4ca1b7d14
Vitor Teixeira <vitor.teixeira@brainz.group>
2022-05-08 20:54:11 -0300

    Criação da página de seleção de connexão sem funções

---
+++  Trabalho1_BD/src/trabalho1_bd/Trabalho1_BD.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package trabalho1_bd;
+
+/**
+ *
+ * @author vitor
+ */
+public class Trabalho1_BD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ChooseConnectionForm().setVisible(true);
+            }
+        });
+    }
+    
+}
